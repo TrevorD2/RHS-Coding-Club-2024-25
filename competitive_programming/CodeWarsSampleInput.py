d314f4f41243a73e3c2f40dcdc7ad8c3c45fae58
Trevor Douglas <145886230+TrevorD2@users.noreply.github.com>
2025-02-12 18:26:44 +0000

    added io

---
+++ competitive_programming/CodeWarsSampleInput.py
@@ -0,0 +1,13 @@
+"""
+with open("competitive_programming/input.txt") as f:
+    cont = f.read().strip()
+
+    print(cont.split("\n"))
+"""
+
+
+import os
+
+files = os.listdir("files")
+
+print(files)
